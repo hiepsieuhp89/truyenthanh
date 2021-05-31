67142672ec3d2248e817a718d5ba99f2009864fd
Tung <tungcan2000@gmail.com>
2021-05-31 22:14:12 +0700

    bt

--- app/Admin/Controllers/StreamController.php
+++ app/Admin/Controllers/StreamController.php
@@ -17,7 +17,7 @@ use App\Admin\Actions\VoiceRecord\BatchDelete;
 
 
 
-class VoiceRecordController extends AdminController
+class StreamController extends AdminController
 {
     /**
      * Title for current resource.
@@ -42,52 +42,7 @@ class VoiceRecordController extends AdminController
      */
     protected function grid()
     {
-        
-
-        return $grid;
-    }
-
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(VoiceRecord::findOrFail($id));
-
-        $show->field('id', __('Id'));
-        $show->field('name', __('Tên'));
-        $show->field('fileVoice', __('FileVoice'));
-        $show->field('creatorId', __('Người tạo'));
-        $show->field('created_at', __('Thời gian tạo'));
-        $show->field('updated_at', __('Thời gian cập nhật'));
-
-        return $show;
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new VoiceRecord());
-
-        $form->text('name', __('Tên bản ghi'));
-
-        $form->record('fileVoice', __('File ghi âm'));
-
-        $form->saving(function ($form) {
-
-            file_put_contents(config('filesystems.disks.upload.path') . 'records/' . $form->fileVoice->getClientOriginalName(), file_get_contents($form->fileVoice));
-
-            $form->fileVoice = 'records/' . $form->fileVoice->getClientOriginalName();
-
-            $form->model()->creatorId = Admin::user()->id;
-        });
-        return $form;
+        $url = config('filesystems.disks.upload.url').'streams/'.(Request::all())['key'].'.m3u8';
+        return view('stream',['url'=>$url]);
     }
 }
