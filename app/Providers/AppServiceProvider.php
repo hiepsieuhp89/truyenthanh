b82a50fc730d2fb06371659d2b2b70e1375adfbc
Tung <tungcan2000@gmail.com>
2021-06-03 16:05:44 +0700

    bt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,25 +1,13 @@
 <?php
 
 namespace App\Providers;
-
-use App;
-use App\Device;
-use App\Admin;
-use App\Area;
 use App\DeviceInfo;
-use App\Api;
-use App\Document;
-use App\Program;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Session;
 use Carbon\Carbon;
-
-use Encore\Admin\Config\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
-    use Api;
     /**
      * Bootstrap any application services.
      *
@@ -29,7 +17,32 @@ class AppServiceProvider extends ServiceProvider
     {
         Carbon::setLocale('vi');
 
-        $response = $this->getDevicesStatus();
+        $curl = curl_init();
+
+        $dataRequest = "eyJEYXRhVHlwZSI6MjAsIkRhdGEiOiJHRVRfQUxMX0RFVklDRV9TVEFUVVMifQ==";
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://103.130.213.161:906/" . $dataRequest,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_CONNECTTIMEOUT => 20,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+        $response = str_replace(':"{', ":{", $response);
+        $response = str_replace(':"[{', ":[{", $response);
+        $response = str_replace('"}"', "}", $response);
+        $response = str_replace('"{"', "{", $response);
+        $response = str_replace(']"}', "]}", $response);
+        $response = json_decode($response, true);
         
         if(isset($response['DataType']) && $response['DataType'] == 5){
 
