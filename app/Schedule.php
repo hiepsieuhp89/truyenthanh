fcb4f0b0421b9704192d78f65c783f657e3e59bb
Tung <tungcan2000@gmail.com>
2021-06-10 16:36:07 +0700

    bt

--- app/Schedule.php
+++ app/Schedule.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace App;
@@ -8,10 +7,9 @@ use Carbon\Carbon;
 
 class Schedule extends Model
 {
-    public function get_schedule_of_device()
-    {
+    public function get_schedule_of_device(){
         $today = Carbon::now('Asia/Ho_Chi_Minh')->toDateString();
-        if ($today > $this->startDate && $today <= $this->endDate)
+        if($today > $this->startDate && $today <= $this->endDate)
             $this->startDate = $today;
 
         return '{\\\\\\\\\\\\\"SongName\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $this->fileVoice . '\\\\\\\\\\\\\",\\\\\\\\\\\\\"TimeStart\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $this->time . '\\\\\\\\\\\\\",\\\\\\\\\\\\\"TimeStop\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $this->endTime . '\\\\\\\\\\\\\",\\\\\\\\\\\\\"DateStart\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $this->startDate . '\\\\\\\\\\\\\",\\\\\\\\\\\\\"DateStop\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $this->endDate . '\\\\\\\\\\\\\",\\\\\\\\\\\\\"PlayType\\\\\\\\\\\\\":1,\\\\\\\\\\\\\"PlayRepeatType\\\\\\\\\\\\\":1}';
