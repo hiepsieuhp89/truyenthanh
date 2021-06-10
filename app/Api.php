e5dc2ac3ec2f0ecb60eda09ddb3ec5f3c7160153
Tung <tungcan2000@gmail.com>
2021-06-10 10:53:26 +0700

    bt

--- app/Api.php
+++ app/Api.php
@@ -210,8 +210,10 @@ trait Api
 
         if ($type == 1 || $type == 4 || $type == 5 || $type == 2) { // nếu phát ngay file pt
             foreach ($deviceCode as $device) {
+                $dataRequest .= '{\"DeviceID\":\"' . trim($device) . '\",\"CommandSend\":\"{\\\\\"Data\\\\\":\\\\\"{\\\\\\\\\\\\\"PlayRepeatType\\\\\\\\\\\\\":1,\\\\\\\\\\\\\"PlayType\\\\\\\\\\\\\":2,\\\\\\\\\\\\\"SongName\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $songName . '\\\\\\\\\\\\\"}\\\\\",\\\\\"PacketType\\\\\":5}\"}';
 
-                $dataRequest .= '{\"DeviceID\":\"' . trim($device) . '\",\"CommandSend\":\"{\\\\\"Data\\\\\":\\\\\"{\\\\\\\\\\\\\"PlayRepeatType\\\\\\\\\\\\\":1,\\\\\\\\\\\\\"PlayType\\\\\\\\\\\\\":2,\\\\\\\\\\\\\"SongName\\\\\\\\\\\\\":\\\\\\\\\\\\\"' . $songName . '\\\\\\\\\\\\\"}\\\\\",\\\\\"PacketType\\\\\":5}\"},';
+                if($device != $deviceCode[count($deviceCode)-1])
+                    $dataRequest .= ',';
             }
         }
 
